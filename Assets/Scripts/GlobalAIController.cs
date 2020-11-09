5fea9117873880195b93be077767a6019f1536cd
Carlisle <carlisle.m@hotmail.com>
2020-11-09 13:11:37 +1000

    Add in campfires and ability to have villagers search and move to the closest one

--- Assets/Scripts/GlobalAIController.cs
+++ Assets/Scripts/GlobalAIController.cs
@@ -8,6 +8,11 @@ public class GlobalAIController : MonoBehaviour
     [SerializeField]
     private GameObject[] villagers;
 
+    [SerializeField]
+    private GameObject[] campFire;
+
+    private bool findFire = false;
+
     void Start()
     {
         System.Random r = new System.Random();
@@ -17,15 +22,57 @@ public class GlobalAIController : MonoBehaviour
             int xInt = r.Next(-14, 22); 
             int yInt = r.Next(-9, 10);
 
-            Debug.Log(xInt);
-            Debug.Log(yInt);
-
             villager.GetComponent<VillagerScript>().waypoint.transform.position = new Vector3(xInt+500, yInt+500, 0);
         }
     }
 
     void Update()
     {
+        if (Input.GetKeyDown("space"))
+        {
+            findFire = true;
+
+            foreach (GameObject villager in villagers)
+            {
+                Transform [] t = villager.GetComponentsInChildren<Transform>();
+                Transform villagerLocation = t[1];
+
+                float closestCampFireDist = 1000000;
+                int closestCampFire = 0;
+
+                for (int i = 0; i < campFire.Length; i++)
+                {  
+                    float dist = Vector3.Distance(campFire[i].transform.position, villagerLocation.transform.position);
+
+                    if (dist < closestCampFireDist)
+                    {
+                       closestCampFireDist = dist;
+                       closestCampFire = i;
+                    }
+                }
+
+                villager.GetComponent<VillagerScript>().waypoint.transform.position = campFire[closestCampFire].transform.position;
+
+                // villager.GetComponent<VillagerScript>().waypoint.transform.position = new Vector3(500, 500, 0);
+            }
+        }
+
+        if (findFire)
+        {
+            foreach (GameObject villager in villagers)
+            {
+                Transform [] t = villager.GetComponentsInChildren<Transform>();
+                Transform villagerLocation = t[1];
+
+                for (int i = 0; i < campFire.Length; i++)
+                {  
+                    if ((Vector3.Distance(campFire[i].transform.position, villagerLocation.transform.position) < 3))
+                    {
+                        villager.GetComponent<VillagerScript>().waypoint.transform.position = villagerLocation.transform.position;
+                    }
+                }
+            }
+        }
         
     }
 }
